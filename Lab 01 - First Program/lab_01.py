44cf9a266220cefbeec0d1fc7b7f15cfaed9aea1
Tenley Schroeder <tenley.schroeder@my.simpson.edu>
2021-09-02 15:16:25 -0500

    Lab 1- Print

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,5 @@
+print("""Once upon a time there was a frog.
+The frog didn't have many friends,\"I wish had someone to sit on this log with me.\" """)
+print("A pixie was flying by and overheard the frog sighing to himself")
+print("\"I will be this friend!\" The pixie thought to herself.")
+print("The pixie headed torwards the frog on the log.")
